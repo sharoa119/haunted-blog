71284906bf829c7bf6940620176468d77cb60b2c
sharoa119 <sau.rin1109@gmail.com>
2025-03-10 14:51:07 +0900

    APIのレスポンス制御

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -3,8 +3,10 @@
 class Api::LikingUsersController < ApplicationController
   def index
     blog = Blog.find(params[:blog_id])
-    @users = blog.liking_users.order(:id)
+    @users = blog.liking_users.order(:id).as_json(except: [:email])
     my_liking = blog.likings.find_by(user: current_user)
     @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
+
+    render json: { users: @users }
   end
 end
