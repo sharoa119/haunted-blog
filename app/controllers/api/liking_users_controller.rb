857f244a7b4dab48ecb1351c7f2e828ac286c5cf
sharoa119 <sau.rin1109@gmail.com>
2025-03-12 16:53:44 +0900

    APIのレスポンス制限のコードを見直す

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -3,10 +3,8 @@
 class Api::LikingUsersController < ApplicationController
   def index
     blog = Blog.find(params[:blog_id])
-    @users = blog.liking_users.order(:id).as_json(except: [:email])
+    @users = blog.liking_users.order(:id)
     my_liking = blog.likings.find_by(user: current_user)
     @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
-
-    render json: { users: @users }
   end
 end
